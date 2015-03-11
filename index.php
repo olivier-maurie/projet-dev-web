cd1d3ee8dc7c2c3597b42a49feaaf8392503dbce
Olivier Maurie <olivier.maurie@y-nov.com>
2015-03-11 10:50:57 +0100

    récupération fonctions

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php include('includes/connexion_bdd.php'); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Projet foot</title>
+    <title>Projet foot | Connexion, inscription</title>
     <link href="css/bootstrap.min.css" rel="stylesheet"/>
 	<link href="css/design.css" rel="stylesheet"/>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
--- pages/accueil.php
+++ pages/accueil.php
@@ -1,9 +1,19 @@
-<?php include("../includes/connexion_bdd.php");?>
-<html>
+<?php include('../includes/connexion_bdd.php'); ?>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-	<title>En Direct du Stade</title>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" href="../styles/moncss.css"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Projet foot | Connexion, inscription</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet"/>
+	<link href="../css/design.css" rel="stylesheet"/>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
 <!-- HEADER -->
