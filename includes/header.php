d1d4d72df4b8b020f4d6b6a97502df814b8a26ca
olivier-maurie <olivier.maurie@gmail.com>
2015-04-07 02:10:30 +0200

    recuperation des fichiers

--- includes/header.php
+++ includes/header.php
@@ -5,13 +5,24 @@
 				<a href="accueil.php"><h1>Resultats Ligue 1</h1></a>
 			</div>
 			<div class="col-lg-3">
-			<a href="../pages/mesparis.php"><h2>Mes Paris</h2></a>
-				<?php
+			<?php
 				echo "bonjour l'ami !";
 				echo "Ton nombre de point est de ".$_SESSION["user"]->getPoint();
-				?>
+			?>
+				<div class="dropdown">
+				<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+					Dropdown
+					<span class="caret"></span>
+				</button>
+					<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+						<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
+						<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
+						<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+						<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+					</ul>
+				</div>
 			</div>
-		</div>
+		</div><a href="../pages/mesparis.php">Mes Paris</a>
 		<div class="mesparis" style="float:right">
 		</div>
 	</div>
--- index.php
+++ index.php
@@ -65,6 +65,7 @@
 			}
 		}
 	?>
+	
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-4 col-md-offset-4">
