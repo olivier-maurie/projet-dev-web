6022ea70572b525c153d6e45b83c559ee1afb2dc
olivier-maurie <olivier.maurie@gmail.com>
2015-04-08 12:05:27 +0200

    j

--- pages/accueil.php
+++ pages/accueil.php
@@ -7,7 +7,7 @@ session_start();?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Projet foot | Connexion, inscription</title>
+    <title>Projet foot | Accueil</title>
     <link href="../css/bootstrap.min.css" rel="stylesheet"/>
 	<link href="../css/design.css" rel="stylesheet"/>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
