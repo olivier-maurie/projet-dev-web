a0e8f32fa76053a9676697958dfcd5dc75a465e4
Jules Basse-Cathalinat <jules.basse-cathalinat@y-nov.com>
2015-04-08 08:39:02 +0200

    commit

---
+++ pages/pageadmin.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf8">
+	</head>
+	<body>
+		<?php
+			require_once "../includes/connexion_bdd.php";
+			require_once "../includes/autoload.inc.php";
+			$sql = "SELECT id, dom, ext, butdom, butext, cotedom, cotenul, coteext FROM resultat";
+			$sql = $db->prepare($sql);
+			$sql->execute();
+			
+			while ($resultat2 = $sql->fetch())
+			{
+				$nul = "nul";
+				?>
+				<div class="row">
+					<div class="col-lg-6 col- col-lg-offset-3">
+						<div class="row">
+							<div class="row">
+								<div class="col-lg-8">
+									<h3><?php echo $resultat2['dom']; ?>  -  <?php echo $resultat2['ext'];?> </h3>
+								</div>
+							
+							<form method="POST" action="" class="parie">
+								<div class="col-lg-4">
+								<ul>
+									<li>
+										<span class="equipe"><?php echo substr($resultat2['dom'], 0, 3); ?></span>
+										<span><input type="radio" name="cote" value=<?php echo $resultat2["dom"];?>></span>
+										<span><?php echo $resultat2["cotedom"];?></span>
+										<?php $dom = $resultat2["cotedom"];?>
+									</li>
+									<li>
+										<span class="equipe">Nul</span>
+										<span><input type="radio" name="cote" value=<?php echo $nul?> checked/></span>
+										<span><?php echo $resultat2["cotenul"];?></span>
+										<?php $nul = $resultat2["cotenul"];?>
+									</li>
+									<li>
+										<span class="equipe"><?php echo substr($resultat2['ext'], 0, 3); ?></span>
+										<span><input type="radio" name="cote" value=<?php echo $resultat2['ext'] ?>></span>
+										<span><?php echo $resultat2["coteext"];?></span>
+										<?php $ext=$resultat2["coteext"];?>
+									</li>
+								</ul>
+								</div>
+							
+										<input type="submit" class="btn btn-default" name="envoyer" value="VALIDER!">
+									
+							</form>
+							
+							
+						</div>
+					</div>
+				</div>
+
+			<?php
+			}
+			?>
+			
+			<?php
+				if(!empty($_POST["envoyer"]))
+				{
+				
+					$match = $_POST["cote"];
+					$req_pari2 = "SELECT id, dom, ext, cotedom, cotenul, coteext, sommeparie, id_user, coteparie, equipe_pari FROM pari WHERE equipe_pari = '".$match."'";
+					$req_pari=$db->prepare($req_pari2);
+					$req_pari->execute();
+					
+					while($gain = $req_pari->fetch())
+					{
+						$user_id = $gain["id_user"];
+						echo "bite";
+						$req_user = $db->prepare("SELECT id, nom, prenom, password, pseudo, points, admin FROM user WHERE id = '".$user_id."'");
+						$req_user->execute();
+						while($columns = $req_user->fetch())
+						{
+							$useract = new user($columns["id"], $columns["nom"], $columns["prenom"], $columns["password"], $columns["pseudo"], $columns["points"]);
+							$useract->victoire($gain["coteparie"], $gain["sommeparie"]);
+							$delete2 = "DELETE FROM pari WHERE id = :id";
+							$params = array(
+								'id' => $gain["id"]
+							);
+
+							$delete = $db->prepare($delete2);
+							$delete->execute($params);							
+						}
+						
+					}
+					
+					
+				}
+			?>
+	</body>
+</html>
